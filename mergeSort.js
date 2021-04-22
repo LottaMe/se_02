e5ae48e0c4a2674e8e4c4249f971d2c6e2490990
Lotta Melcher <lotta.melcher@code.berlin>
2021-04-22 14:41:27 +0200

    add mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -1,10 +1,30 @@
-// p = beginning of subarray
-// r = ending of subarray
-function mergeSort(array, p, r) {
-    if (p < r) {
-        let q = Math.floor((p + r) / 2);
-        mergeSort(array, p, q);
-        mergeSort(array, q + 1, r);
-        merge(array, p, q, r);
+function mergeSort(array) {
+    const half = array.length / 2;
+    // Base case
+    if(array.length < 2){
+      return array;
     }
-};
+    const left = array.splice(0, half);
+    return merge(mergeSort(left),mergeSort(array));
+}
+
+function merge(left, right) {
+    let arr = [];
+    // Loop ends if one of the two arrays is empty
+    while (left.length && right.length) {
+        // Pick the smaller among the smallest element of left and right sub arrays 
+        // The subarrays are already sorted. shift() also removes the first element
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    
+    // Concatenating the leftover elements (either left or right is already empty)
+    return [ ...arr, ...left, ...right ];
+}
+
+let test = [14, 2, 9, 12, 11, 7, 3, 6];
+console.log(mergeSort(test));
+console.log(" should equal 2, 3, 6, 7, 9, 11, 12, 14")
